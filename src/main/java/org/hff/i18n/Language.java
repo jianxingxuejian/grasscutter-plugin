83582dcd1dfde291870753e23b41cd6678e2e579
null <975959057@qq.com>
2023-01-21 00:18:06 +0800

    fix bug

--- src/main/java/org/hff/i18n/Language.java
+++ src/main/java/org/hff/i18n/Language.java
@@ -3,21 +3,29 @@ package org.hff.i18n;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import emu.grasscutter.game.Account;
+import emu.grasscutter.utils.FileUtils;
 import emu.grasscutter.utils.Utils;
 import org.hff.MyPlugin;
 import org.hff.api.ApiCode;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
+import static emu.grasscutter.Grasscutter.getLogger;
 
 public final class Language {
 
     private final Map<ApiCode, String> cachedMsg = new ConcurrentHashMap<>();
     private final Map<String, String> cachedMail = new ConcurrentHashMap<>();
 
-    private final JsonObject msgData;
-    private final JsonObject mailData;
+    private JsonObject msgData;
+    private JsonObject mailData;
 
     public String getMsg(ApiCode code) {
         if (cachedMsg.containsKey(code)) {
@@ -48,17 +56,30 @@ public final class Language {
         } catch (Exception e) {
             return null;
         }
-
         cachedMail.put(key, text);
         return text;
     }
 
     public Language(Locale locale) {
         InputStream inputStream = Language.class.getResourceAsStream("/locales/" + locale.getDesc() + ".json");
-        String read = Utils.readFromInputStream(inputStream);
-        JsonObject jsonObject = MyPlugin.gson.fromJson(read, JsonObject.class);
-        this.msgData = jsonObject.getAsJsonObject("msg");
-        this.mailData = jsonObject.getAsJsonObject("mail");
+        if (inputStream == null) {
+            // If inputStream is null, use the jar loader to read
+            String version = getClass().getPackage().getImplementationVersion();
+            File pluginFile = FileUtils.getPluginPath("grasscutter-plugin-" + version + ".jar").toFile();
+            try {
+                URL url = pluginFile.toURI().toURL();
+                try (URLClassLoader loader = new URLClassLoader(new URL[]{url})) {
+                    InputStream is = loader.findResource("locales/" + locale.getDesc() + ".json").openStream();
+                    setData(is);
+                } catch (IOException e) {
+                    getLogger().error("fail to load locale");
+                }
+            } catch (MalformedURLException exception) {
+                getLogger().error("fail to convert to url");
+            }
+        } else {
+            setData(inputStream);
+        }
     }
 
     public static Locale getLocale(Account account) {
@@ -67,5 +88,12 @@ public final class Language {
         } else {
             return Locale.en;
         }
+    }
+
+    private void setData(InputStream inputStream) {
+        String read = Utils.readFromInputStream(inputStream);
+        JsonObject jsonObject = MyPlugin.gson.fromJson(read, JsonObject.class);
+        this.msgData = jsonObject.getAsJsonObject("msg");
+        this.mailData = jsonObject.getAsJsonObject("mail");
     }
 }
